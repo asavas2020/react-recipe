3dcd9d72f32c505068eccf8f2273abe47c42ba53
Gokhan <gknsvsdata@gmail.com>
2022-07-14 21:29:01 +0300

    home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,28 @@
+import { Select } from "@material-ui/core"
 
-const Header = () => {
-  return (
-    <div>Header</div>
+
+const Header = ({setQuery, setMeal, mealTypes, getData}) => {
+  return ( <HeaderContainer>
+  <MainHeader>Recipe App</MainHeader>
+    <FormContainer>
+      <FoodInput 
+      type="text" 
+      placeholder="search" 
+      onChange={(e)=> setQuery(e.target.value)}
+      />
+      <Button type="submit">SEARCH</Button>
+      <Select
+      name="mealTypes"
+      id="mealTypes"
+      onChange={(e)=> setMeal(e.target.value)}>
+        {mealTypes.map((meal,index)=>(
+          <option key ={index} value ={meal.toLowerCase()}>
+            {meal}
+          </option>
+        ))}
+      </Select>
+    </FormContainer>
+  </HeaderContainer>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,32 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import Header from "../../components/header/Header";
+import Cards from "../../components/cards/Cards"
+
 
 const APP_ID = "6128d0b9"
 const APP_KEY = "44670ca855dadffdcb127e78b9e53980"	
 
 const Home = () => {
-  const [query, setQuery] = useState("");
-  const [meal, setMeal] = useState("")
+  const mealTypes=["Breakfast","Lunch","Dinner","Snack","Teatime"];
+  const [query, setQuery] = useState("egg");
+  const [meal, setMeal] = useState("dinner");
+  const [recipe, setRecipe] = useState("")
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`
 
 
-  const getData =async () =>{
-    const data = await axios.get(url);
-    console.log(data)
+  const getData = async () =>{
+    const {data} = await axios.get(url);
+   setRecipe(data.hits)
   };
-  getData()
   
-  useEffect(() => {
-    getData();   
-
-  }, [])
-  
-
   return (
-    <div>Home</div>
+    <div>
+      <Header setQuery={setQuery} setMeal={setMeal} mealTypes={mealTypes} getData={getData}/>
+      <Cards/>
+    </div>
   )
 }
 
